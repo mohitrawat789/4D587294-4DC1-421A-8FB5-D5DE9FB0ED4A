3cc1063e96ab616cb3b3caa8eff6a833dd02e998
Gaurav Khandelwal <Gaurav.Khandelwal@jci.com>
2018-09-03 11:47:27 +0530

    Add master page. This page will read Serial Port attached to your device.

---
+++ 4D587294-4DC1-421A-8FB5-D5DE9FB0ED4A/Parking/MasterPage.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace Parking
+{
+    public partial class MasterPage : Form
+    {
+        private const string NewLine = "\n";
+
+        public MasterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGetSerialPortsClick(object sender, EventArgs e)
+        {
+            string[] arrayComPortsNames = null;
+            int index = -1;
+            string comPortName = null;
+
+            arrayComPortsNames = SerialPort.GetPortNames();
+            do
+            {
+                index += 1;
+                rtbIncomingData.Text += arrayComPortsNames[index] + NewLine;
+
+            } while (!(arrayComPortsNames[index] == comPortName || index == arrayComPortsNames.GetUpperBound(0)));
+        }
+    }
+}
