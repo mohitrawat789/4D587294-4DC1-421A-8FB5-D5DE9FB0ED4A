0fff3f257acb72e364c94d2067a0b2ea7d62218a
Gaurav Khandelwal <Gaurav.Khandelwal@jci.com>
2018-09-06 23:25:11 +0530

    Add serial port setting combo-box

--- 4D587294-4DC1-421A-8FB5-D5DE9FB0ED4A/Parking/MasterPage.Designer.cs
+++ 4D587294-4DC1-421A-8FB5-D5DE9FB0ED4A/Parking/MasterPage.Designer.cs
@@ -30,6 +30,13 @@
         {
             this.btnGetSerialPorts = new System.Windows.Forms.Button();
             this.rtbIncomingData = new System.Windows.Forms.RichTextBox();
+            this.cboPorts = new System.Windows.Forms.ComboBox();
+            this.cboBaudRate = new System.Windows.Forms.ComboBox();
+            this.cboDataBits = new System.Windows.Forms.ComboBox();
+            this.cboStopBits = new System.Windows.Forms.ComboBox();
+            this.cboParity = new System.Windows.Forms.ComboBox();
+            this.cboHandShaking = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnGetSerialPorts
@@ -38,23 +45,87 @@
             this.btnGetSerialPorts.Name = "btnGetSerialPorts";
             this.btnGetSerialPorts.Size = new System.Drawing.Size(146, 35);
             this.btnGetSerialPorts.TabIndex = 0;
-            this.btnGetSerialPorts.Text = "Ports";
+            this.btnGetSerialPorts.Text = "Serial Port Settings";
             this.btnGetSerialPorts.UseVisualStyleBackColor = true;
             this.btnGetSerialPorts.Click += new System.EventHandler(this.BtnGetSerialPortsClick);
             // 
             // rtbIncomingData
             // 
-            this.rtbIncomingData.Location = new System.Drawing.Point(64, 141);
+            this.rtbIncomingData.Location = new System.Drawing.Point(64, 324);
             this.rtbIncomingData.Name = "rtbIncomingData";
-            this.rtbIncomingData.Size = new System.Drawing.Size(664, 259);
+            this.rtbIncomingData.Size = new System.Drawing.Size(664, 76);
             this.rtbIncomingData.TabIndex = 1;
             this.rtbIncomingData.Text = "";
+            // 
+            // cboPorts
+            // 
+            this.cboPorts.FormattingEnabled = true;
+            this.cboPorts.Location = new System.Drawing.Point(257, 41);
+            this.cboPorts.Name = "cboPorts";
+            this.cboPorts.Size = new System.Drawing.Size(196, 24);
+            this.cboPorts.TabIndex = 2;
+            // 
+            // cboBaudRate
+            // 
+            this.cboBaudRate.FormattingEnabled = true;
+            this.cboBaudRate.Location = new System.Drawing.Point(257, 83);
+            this.cboBaudRate.Name = "cboBaudRate";
+            this.cboBaudRate.Size = new System.Drawing.Size(196, 24);
+            this.cboBaudRate.TabIndex = 3;
+            // 
+            // cboDataBits
+            // 
+            this.cboDataBits.FormattingEnabled = true;
+            this.cboDataBits.Location = new System.Drawing.Point(257, 126);
+            this.cboDataBits.Name = "cboDataBits";
+            this.cboDataBits.Size = new System.Drawing.Size(196, 24);
+            this.cboDataBits.TabIndex = 4;
+            // 
+            // cboStopBits
+            // 
+            this.cboStopBits.FormattingEnabled = true;
+            this.cboStopBits.Location = new System.Drawing.Point(257, 170);
+            this.cboStopBits.Name = "cboStopBits";
+            this.cboStopBits.Size = new System.Drawing.Size(196, 24);
+            this.cboStopBits.TabIndex = 5;
+            // 
+            // cboParity
+            // 
+            this.cboParity.FormattingEnabled = true;
+            this.cboParity.Location = new System.Drawing.Point(257, 210);
+            this.cboParity.Name = "cboParity";
+            this.cboParity.Size = new System.Drawing.Size(196, 24);
+            this.cboParity.TabIndex = 6;
+            // 
+            // cboHandShaking
+            // 
+            this.cboHandShaking.FormattingEnabled = true;
+            this.cboHandShaking.Location = new System.Drawing.Point(257, 252);
+            this.cboHandShaking.Name = "cboHandShaking";
+            this.cboHandShaking.Size = new System.Drawing.Size(196, 24);
+            this.cboHandShaking.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(651, 425);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // MasterPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(947, 481);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cboHandShaking);
+            this.Controls.Add(this.cboParity);
+            this.Controls.Add(this.cboStopBits);
+            this.Controls.Add(this.cboDataBits);
+            this.Controls.Add(this.cboBaudRate);
+            this.Controls.Add(this.cboPorts);
             this.Controls.Add(this.rtbIncomingData);
             this.Controls.Add(this.btnGetSerialPorts);
             this.Name = "MasterPage";
@@ -67,6 +138,13 @@
 
         private System.Windows.Forms.Button btnGetSerialPorts;
         private System.Windows.Forms.RichTextBox rtbIncomingData;
+        private System.Windows.Forms.ComboBox cboPorts;
+        private System.Windows.Forms.ComboBox cboBaudRate;
+        private System.Windows.Forms.ComboBox cboDataBits;
+        private System.Windows.Forms.ComboBox cboStopBits;
+        private System.Windows.Forms.ComboBox cboParity;
+        private System.Windows.Forms.ComboBox cboHandShaking;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- 4D587294-4DC1-421A-8FB5-D5DE9FB0ED4A/Parking/MasterPage.cs
+++ 4D587294-4DC1-421A-8FB5-D5DE9FB0ED4A/Parking/MasterPage.cs
@@ -7,8 +7,6 @@ namespace Parking
 {
     public partial class MasterPage : Form
     {
-        private const string NewLine = "\n";
-
         public MasterPage()
         {
             InitializeComponent();
@@ -16,17 +14,101 @@ namespace Parking
 
         private void BtnGetSerialPortsClick(object sender, EventArgs e)
         {
-            string[] arrayComPortsNames = null;
-            int index = -1;
-            string comPortName = null;
+            const int index = -1;
+            var arrayComPortsNames = SerialPort.GetPortNames();
 
-            arrayComPortsNames = SerialPort.GetPortNames();
+            SetPortNames(index, arrayComPortsNames, null);
+            SetBaudRate();
+            SetDataBits();
+            SetStopBits();
+            SetParityBits();
+            SetHandShake();
+        }
+
+        private void SetHandShake()
+        {
+            //Handshake
+            cboHandShaking.Items.Add("None");
+            cboHandShaking.Items.Add("XOnXOff");
+            cboHandShaking.Items.Add("RequestToSend");
+            cboHandShaking.Items.Add("RequestToSendXOnXOff");
+
+            //get the first item print it in the text 
+            cboHandShaking.Text = cboHandShaking.Items[0].ToString();
+        }
+
+        private void SetParityBits()
+        {
+            //Parity 
+            cboParity.Items.Add("None");
+            cboParity.Items.Add("Even");
+            cboParity.Items.Add("Mark");
+            cboParity.Items.Add("Odd");
+            cboParity.Items.Add("Space");
+
+            //get the first item print in the text
+
+            cboParity.Text = cboParity.Items[0].ToString();
+        }
+
+        private void SetStopBits()
+        {
+            //Stop Bits
+            cboStopBits.Items.Add("One");
+            cboStopBits.Items.Add("OnePointFive");
+            cboStopBits.Items.Add("Two");
+            //get the first item print in the text
+            cboStopBits.Text = cboStopBits.Items[0].ToString();
+        }
+
+        private void SetDataBits()
+        {
+            //Data Bits
+            cboDataBits.Items.Add(7);
+            cboDataBits.Items.Add(8);
+            //get the first item print it in the text 
+            cboDataBits.Text = cboDataBits.Items[0].ToString();
+        }
+
+        private void SetBaudRate()
+        {
+            cboBaudRate.Items.Add(300);
+            cboBaudRate.Items.Add(600);
+            cboBaudRate.Items.Add(1200);
+            cboBaudRate.Items.Add(2400);
+            cboBaudRate.Items.Add(9600);
+            cboBaudRate.Items.Add(14400);
+            cboBaudRate.Items.Add(19200);
+            cboBaudRate.Items.Add(38400);
+            cboBaudRate.Items.Add(57600);
+            cboBaudRate.Items.Add(115200);
+            cboBaudRate.Items.ToString();
+
+            //get first item print in text
+            cboBaudRate.Text = cboBaudRate.Items[0].ToString();
+        }
+
+        /// <summary>
+        /// Set port names
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="arrayComPortsNames"></param>
+        /// <param name="comPortName"></param>
+        private void SetPortNames(int index, string[] arrayComPortsNames, string comPortName)
+        {
             do
             {
                 index += 1;
-                rtbIncomingData.Text += arrayComPortsNames[index] + NewLine;
-
+                cboPorts.Items.Add(arrayComPortsNames[index]);
             } while (!(arrayComPortsNames[index] == comPortName || index == arrayComPortsNames.GetUpperBound(0)));
+
+            //want to get first out
+            if (index == arrayComPortsNames.GetUpperBound(0))
+            {
+                comPortName = arrayComPortsNames[0];
+            }
+
+            cboPorts.Text = comPortName;
         }
     }
 }
