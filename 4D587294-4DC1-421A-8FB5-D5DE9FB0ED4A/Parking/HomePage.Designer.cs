6052a1c16002c3c17c6b79b3ae3542a6fee29c0f
Gaurav Khandelwal <Gaurav.Khandelwal@jci.com>
2018-09-07 00:42:12 +0530

    Add Welcome Text on home page

--- 4D587294-4DC1-421A-8FB5-D5DE9FB0ED4A/Parking/HomePage.Designer.cs
+++ 4D587294-4DC1-421A-8FB5-D5DE9FB0ED4A/Parking/HomePage.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnSettings = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnSettings
@@ -41,20 +42,35 @@
             this.btnSettings.UseVisualStyleBackColor = true;
             this.btnSettings.Click += new System.EventHandler(this.BtnSettingsClick);
             // 
-            // Home_Page
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(252, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "NDMC Parking";
+            // 
+            // HomePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSettings);
             this.Name = "HomePage";
             this.Text = "Home_Page";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Label label1;
     }
 }
