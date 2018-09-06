69b287cc4f7699f1c1272f48cdc4c8bf5507788a
Gaurav Khandelwal <Gaurav.Khandelwal@jci.com>
2018-09-07 00:10:39 +0530

    Design Printer Ticket Settings

--- 4D587294-4DC1-421A-8FB5-D5DE9FB0ED4A/Parking/MasterPage.Designer.cs
+++ 4D587294-4DC1-421A-8FB5-D5DE9FB0ED4A/Parking/MasterPage.Designer.cs
@@ -37,13 +37,32 @@
             this.cboParity = new System.Windows.Forms.ComboBox();
             this.cboHandShaking = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.labelBaudRates = new System.Windows.Forms.Label();
+            this.labelDataBits = new System.Windows.Forms.Label();
+            this.labelStopBits = new System.Windows.Forms.Label();
+            this.labelParityBits = new System.Windows.Forms.Label();
+            this.labelHandShaking = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnGetSerialPorts
             // 
-            this.btnGetSerialPorts.Location = new System.Drawing.Point(64, 41);
+            this.btnGetSerialPorts.Location = new System.Drawing.Point(85, 12);
             this.btnGetSerialPorts.Name = "btnGetSerialPorts";
-            this.btnGetSerialPorts.Size = new System.Drawing.Size(146, 35);
+            this.btnGetSerialPorts.Size = new System.Drawing.Size(196, 35);
             this.btnGetSerialPorts.TabIndex = 0;
             this.btnGetSerialPorts.Text = "Serial Port Settings";
             this.btnGetSerialPorts.UseVisualStyleBackColor = true;
@@ -51,16 +70,16 @@
             // 
             // rtbIncomingData
             // 
-            this.rtbIncomingData.Location = new System.Drawing.Point(64, 324);
+            this.rtbIncomingData.Location = new System.Drawing.Point(64, 343);
             this.rtbIncomingData.Name = "rtbIncomingData";
-            this.rtbIncomingData.Size = new System.Drawing.Size(664, 76);
+            this.rtbIncomingData.Size = new System.Drawing.Size(808, 67);
             this.rtbIncomingData.TabIndex = 1;
             this.rtbIncomingData.Text = "";
             // 
             // cboPorts
             // 
             this.cboPorts.FormattingEnabled = true;
-            this.cboPorts.Location = new System.Drawing.Point(257, 41);
+            this.cboPorts.Location = new System.Drawing.Point(146, 63);
             this.cboPorts.Name = "cboPorts";
             this.cboPorts.Size = new System.Drawing.Size(196, 24);
             this.cboPorts.TabIndex = 2;
@@ -68,7 +87,7 @@
             // cboBaudRate
             // 
             this.cboBaudRate.FormattingEnabled = true;
-            this.cboBaudRate.Location = new System.Drawing.Point(257, 83);
+            this.cboBaudRate.Location = new System.Drawing.Point(146, 106);
             this.cboBaudRate.Name = "cboBaudRate";
             this.cboBaudRate.Size = new System.Drawing.Size(196, 24);
             this.cboBaudRate.TabIndex = 3;
@@ -76,7 +95,7 @@
             // cboDataBits
             // 
             this.cboDataBits.FormattingEnabled = true;
-            this.cboDataBits.Location = new System.Drawing.Point(257, 126);
+            this.cboDataBits.Location = new System.Drawing.Point(146, 148);
             this.cboDataBits.Name = "cboDataBits";
             this.cboDataBits.Size = new System.Drawing.Size(196, 24);
             this.cboDataBits.TabIndex = 4;
@@ -84,7 +103,7 @@
             // cboStopBits
             // 
             this.cboStopBits.FormattingEnabled = true;
-            this.cboStopBits.Location = new System.Drawing.Point(257, 170);
+            this.cboStopBits.Location = new System.Drawing.Point(146, 189);
             this.cboStopBits.Name = "cboStopBits";
             this.cboStopBits.Size = new System.Drawing.Size(196, 24);
             this.cboStopBits.TabIndex = 5;
@@ -92,7 +111,7 @@
             // cboParity
             // 
             this.cboParity.FormattingEnabled = true;
-            this.cboParity.Location = new System.Drawing.Point(257, 210);
+            this.cboParity.Location = new System.Drawing.Point(146, 233);
             this.cboParity.Name = "cboParity";
             this.cboParity.Size = new System.Drawing.Size(196, 24);
             this.cboParity.TabIndex = 6;
@@ -100,25 +119,204 @@
             // cboHandShaking
             // 
             this.cboHandShaking.FormattingEnabled = true;
-            this.cboHandShaking.Location = new System.Drawing.Point(257, 252);
+            this.cboHandShaking.Location = new System.Drawing.Point(146, 280);
             this.cboHandShaking.Name = "cboHandShaking";
             this.cboHandShaking.Size = new System.Drawing.Size(196, 24);
             this.cboHandShaking.TabIndex = 7;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(651, 425);
+            this.button1.Location = new System.Drawing.Point(424, 428);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(85, 41);
             this.button1.TabIndex = 8;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(38, 70);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(82, 17);
+            this.labelPort.TabIndex = 9;
+            this.labelPort.Text = "Port Names";
+            // 
+            // labelBaudRates
+            // 
+            this.labelBaudRates.AutoSize = true;
+            this.labelBaudRates.Location = new System.Drawing.Point(38, 113);
+            this.labelBaudRates.Name = "labelBaudRates";
+            this.labelBaudRates.Size = new System.Drawing.Size(82, 17);
+            this.labelBaudRates.TabIndex = 10;
+            this.labelBaudRates.Text = "Baud Rates";
+            // 
+            // labelDataBits
+            // 
+            this.labelDataBits.AutoSize = true;
+            this.labelDataBits.Location = new System.Drawing.Point(38, 155);
+            this.labelDataBits.Name = "labelDataBits";
+            this.labelDataBits.Size = new System.Drawing.Size(65, 17);
+            this.labelDataBits.TabIndex = 11;
+            this.labelDataBits.Text = "Data Bits";
+            // 
+            // labelStopBits
+            // 
+            this.labelStopBits.AutoSize = true;
+            this.labelStopBits.Location = new System.Drawing.Point(38, 196);
+            this.labelStopBits.Name = "labelStopBits";
+            this.labelStopBits.Size = new System.Drawing.Size(64, 17);
+            this.labelStopBits.TabIndex = 12;
+            this.labelStopBits.Text = "Stop Bits";
+            // 
+            // labelParityBits
+            // 
+            this.labelParityBits.AutoSize = true;
+            this.labelParityBits.Location = new System.Drawing.Point(38, 240);
+            this.labelParityBits.Name = "labelParityBits";
+            this.labelParityBits.Size = new System.Drawing.Size(71, 17);
+            this.labelParityBits.TabIndex = 13;
+            this.labelParityBits.Text = "Parity Bits";
+            // 
+            // labelHandShaking
+            // 
+            this.labelHandShaking.AutoSize = true;
+            this.labelHandShaking.Location = new System.Drawing.Point(41, 280);
+            this.labelHandShaking.Name = "labelHandShaking";
+            this.labelHandShaking.Size = new System.Drawing.Size(97, 17);
+            this.labelHandShaking.TabIndex = 14;
+            this.labelHandShaking.Text = "Hand Shaking";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(468, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 17);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Four Wheeler Vehicle Charges / hour";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(468, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 17);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Two Wheeler Vehicle Charges / hour";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(468, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 17);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Lost Ticket Penalty Charges";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(468, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 17);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Ticket Title Display Text";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(468, 233);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(187, 17);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Ticket Title Sub Display Text";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(468, 280);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(200, 17);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Ticket Below Parking Message";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(726, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 22);
+            this.textBox1.TabIndex = 21;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(726, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(146, 22);
+            this.textBox2.TabIndex = 22;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(726, 145);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(146, 22);
+            this.textBox3.TabIndex = 23;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(726, 184);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(146, 22);
+            this.textBox4.TabIndex = 24;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(726, 228);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(146, 22);
+            this.textBox5.TabIndex = 25;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(726, 275);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(146, 22);
+            this.textBox6.TabIndex = 26;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(576, 27);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(175, 20);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "Ticket Printer Settings";
             // 
             // MasterPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(947, 481);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelHandShaking);
+            this.Controls.Add(this.labelParityBits);
+            this.Controls.Add(this.labelStopBits);
+            this.Controls.Add(this.labelDataBits);
+            this.Controls.Add(this.labelBaudRates);
+            this.Controls.Add(this.labelPort);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.cboHandShaking);
             this.Controls.Add(this.cboParity);
@@ -131,6 +329,7 @@
             this.Name = "MasterPage";
             this.Text = "MasterPage";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -145,6 +344,25 @@
         private System.Windows.Forms.ComboBox cboParity;
         private System.Windows.Forms.ComboBox cboHandShaking;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.Label labelBaudRates;
+        private System.Windows.Forms.Label labelDataBits;
+        private System.Windows.Forms.Label labelStopBits;
+        private System.Windows.Forms.Label labelParityBits;
+        private System.Windows.Forms.Label labelHandShaking;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
